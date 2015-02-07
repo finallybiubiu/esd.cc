06ed3f619c94dd138d2a222e155403f78645b310
imlonghao <imlonghao@esd.cc>
2015-02-07 18:32:45 +0800

    Disable /admin folder

--- esdcc/urls.py
+++ esdcc/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'esdcc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.index'),
     url(r'^segmentfault/$', 'main.views.segmentfault_index'),
     url(r'^segmentfault_blog/$', 'main.views.segmentfault_blog_index'),
