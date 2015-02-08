aad7c2ee0ee9e5a41505b9b762bad26a63264031
imlonghao <imlonghao@esd.cc>
2015-02-08 15:23:20 +0800

    [UI] Fix 404 BUG #2

--- esdcc/settings.py
+++ esdcc/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://esd.cc/static/'
 STATIC_ROOT = '/var/www/esd.cc/static/'
 
 
